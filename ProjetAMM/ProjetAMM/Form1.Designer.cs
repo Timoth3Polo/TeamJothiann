95e8806c88fc2b79d22f46e36d1f20fb15bb1b72
Timoth3Polo <timothepaul@orange.fr>
2020-11-06 16:54:03 +0100

    Version 1

---
+++ ProjetAMM/ProjetAMM/Form1.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace ProjetAMM
+{
+    partial class frm_accueil
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frm_accueil
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(825, 450);
+            this.IsMdiContainer = true;
+            this.Name = "frm_accueil";
+            this.Text = "AMM Application de gestion et de suivi des Autorisations de Mise sur le Marché de" +
+    "s médicaments";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ README.txt
@@ -0,0 +1,4 @@
+Pour les forms => frm_votreFenêtre
+Exemple : pour accueil => frm_accueil
+
+Des variables explicites (à rallonge s'il le faut)
